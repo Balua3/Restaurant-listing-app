e4f1049fdc42e1efc3037dc37ed9d7be4897b49c
Balu <baluajayakumar@gmail.com>
2023-03-02 00:28:22 +0530

    Restaurant-listing-app

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function Footer() {
+  return (
+    <div><Card className="text-center">
+    <Card.Header>Address</Card.Header>
+    <Card.Body>
+      <Card.Title>Kochi</Card.Title>
+      <Card.Text>
+        With supporting text below as a natural lead-in to additional content.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer className="text-muted">Copyright © 2018 Tonico Cafe. All Rights Reserved</Card.Footer>
+  </Card></div>
+  )
+}
+
+export default Footer
---
+++ src/components/Viewrest.js
@@ -0,0 +1,71 @@
+import React,{useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Image } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Restop from './Restop';
+import Restreview from './Restreview';
+
+
+function Viewrest() {
+
+    const urlparams=useParams()
+    console.log(urlparams); //{id: '3'}
+    console.log(urlparams.id);//3
+
+    //create a state
+    const [AllRestaurants,setAllRestaurants] = useState([])
+
+    //function to call API to get data from restaurants.json
+
+        const getRestaurants=async()=>{
+            await fetch('/restaurants.json')
+            .then((data)=>{
+                data.json()
+                .then((result)=>{
+                    // console.log(result); //array of data in console
+                    setAllRestaurants(result.restaurants) //(10)
+                })
+            })
+        }
+        console.log(AllRestaurants);
+
+        useEffect(()=>{
+            getRestaurants()
+        },[])
+
+    
+        const viewrest=AllRestaurants.find(item=>item.id==urlparams.id)
+        console.log(viewrest);
+
+  return (
+    <>
+    {
+        viewrest?(
+            <Row className='m-3'>
+                <Col className='ms-5' md={4}>
+                    <Image src={viewrest.photograph} fluid/>
+                </Col>
+                <Col md={7}>
+                <ListGroup>
+                    <ListGroup.Item><h1>{viewrest.name}</h1></ListGroup.Item>
+                    <ListGroup.Item><h3>{viewrest.cuisine_type}</h3></ListGroup.Item>
+                    <ListGroup.Item><h3>{viewrest.neighborhood}</h3></ListGroup.Item>
+                    <ListGroup.Item><h3>{viewrest.address}</h3></ListGroup.Item>
+
+                    <ListGroup.Item><Restop operate={viewrest.operating_hours}/></ListGroup.Item>
+                    
+                    <Restreview review = {viewrest.reviews}/>
+
+                </ListGroup>
+                </Col>
+            </Row>
+        ):'null'
+    }
+    </>
+  )
+}
+
+export default Viewrest
